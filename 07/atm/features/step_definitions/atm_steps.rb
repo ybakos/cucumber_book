aa6b51d3629b29b47c2d2c820416999784964cde
Yong Bakos <ybakos@humanoriented.com>
2012-07-27 16:22:18 -0600

    Passing first two scenarios. Chapter 7.3

--- 07/atm/features/step_definitions/atm_steps.rb
+++ 07/atm/features/step_definitions/atm_steps.rb
@@ -10,19 +10,34 @@ class Account
 
 end
 
+class Teller
+
+  def withdraw_from(account, amount)
+
+  end
+
+end
+
+# helper method
+module KnowsMyAccount
+  def my_account
+    @my_account ||= Account.new
+  end
+end
+World(KnowsMyAccount)
 
 CAPTURE_CASH_AMOUNT = Transform /^\$(\d+)$/ do |number|
                         number.to_i
                       end
 
 Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
-  my_account = Account.new
   my_account.deposit(amount)
   my_account.balance.should eq(amount), "Expected the balance to be #{amount} but was #{my_account.balance}."
 end
 
-When /^I request (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
-  pending # express the regexp above with the code you wish you had
+When /^I withdraw (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
+  teller = Teller.new
+  teller.withdraw_from(my_account, amount)
 end
 
 Then /^\$(\d+) should be dispensed$/ do |arg1|
