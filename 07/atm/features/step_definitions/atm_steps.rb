aec86a8e341d74e4d972d0c03720146f13c66f7e
Yong Bakos <ybakos@humanoriented.com>
2012-07-26 22:58:03 -0600

    Adding simple passing implementation for first atm step. Chapter 7.1

--- 07/atm/features/step_definitions/atm_steps.rb
+++ 07/atm/features/step_definitions/atm_steps.rb
@@ -1,5 +1,19 @@
-Given /^I have deposited \$(\d+) in my account$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+class Account
+
+  def deposit(amount)
+    @balance = amount
+  end
+
+  def balance
+    @balance
+  end
+
+end
+
+Given /^I have deposited \$(\d+) in my account$/ do |amount|
+  my_account = Account.new
+  my_account.deposit(amount.to_i)
+  my_account.balance.should eq(amount.to_i), "Expected the balance to be #{amount} but was #{my_account.balance}."
 end
 
 When /^I request \$(\d+)$/ do |arg1|
