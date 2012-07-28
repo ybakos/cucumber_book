12f55c709662dacac956b756baf08fc91368041a
Yong Bakos <ybakos@humanoriented.com>
2012-07-28 07:20:51 -0600

    Adding teller action of actually debiting amount from account. Renaming step and methods to reflect domain of crediting account. Chapter 8.1

--- 0708/atm/features/step_definitions/account_steps.rb
+++ 0708/atm/features/step_definitions/account_steps.rb
@@ -1,4 +1,7 @@
-Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
-  my_account.deposit(amount)
+Given /^my account has been credited with (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
+  my_account.credit(amount)
+end
+
+Then /^the balance of my account should be (#{CAPTURE_CASH_AMOUNT})$/ do |amount|
   my_account.balance.should eq(amount), "Expected the balance to be #{amount} but was #{my_account.balance}."
 end
--- 0708/atm/lib/nice_bank.rb
+++ 0708/atm/lib/nice_bank.rb
@@ -1,11 +1,15 @@
 class Account
 
-  def deposit(amount)
+  def credit(amount)
     @balance = amount
   end
 
   def balance
     @balance
+  end
+
+  def debit(amount)
+    @balance -= amount
   end
 
 end
@@ -17,6 +21,7 @@ class Teller
   end
 
   def withdraw_from(account, amount)
+    account.debit(amount)
     @cash_slot.dispense(amount)
   end
 
