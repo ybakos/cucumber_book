bc3bd0a78a553e1d9a4eb1203a28759c0bd333f4
Yong Bakos <ybakos@humanoriented.com>
2012-07-30 13:14:49 -0600

    Adding transaction elements from chapter 9.3.

--- 09/atm/features/support/hooks.rb
+++ 09/atm/features/support/hooks.rb
@@ -2,3 +2,8 @@ After do
   # Customer takes cash, leaving drawer empty
   cash_slot.contents = 0
 end
+
+Before do
+  BalanceStore.new.balance = 0
+  TransactionQueue.clear
+end
--- 09/atm/lib/nice_bank.rb
+++ 09/atm/lib/nice_bank.rb
@@ -1,15 +1,23 @@
+require_relative 'transaction_queue'
+require_relative 'balance_store'
+
 class Account
 
+  def initialize
+    @queue = TransactionQueue.new
+    @balance_store = BalanceStore.new
+  end
+
   def credit(amount)
-    @balance = amount
+    @queue.write("+#{amount}")
   end
 
   def balance
-    @balance
+    @balance_store.balance
   end
 
   def debit(amount)
-    @balance -= amount
+    @queue.write("-#{amount}")
   end
 
 end
