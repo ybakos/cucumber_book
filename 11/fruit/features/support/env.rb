8886c442df683d08eed4963ac8529127d0de270f
Yong Bakos <ybakos@humanoriented.com>
2012-08-10 11:44:38 -0600

    Adding passing step definitions using rack-test. Chapter 12.1

--- 11/fruit/features/support/env.rb
+++ 11/fruit/features/support/env.rb
@@ -1,1 +1,12 @@
 require File.join(File.dirname(__FILE__), '..', '..', 'fruit_app')
+require 'rack/test'
+
+module AppHelper
+
+  def app
+    FruitApp
+  end
+
+end
+
+World(Rack::Test::Methods, AppHelper)
--- 11/fruit/fruit_app.rb
+++ 11/fruit/fruit_app.rb
@@ -2,4 +2,10 @@ require 'sinatra'
 
 class FruitApp < Sinatra::Base
   set :data, ''
+
+  get '/fruits' do
+    content_type :json
+    FruitApp.data.to_json
+  end
+
 end
