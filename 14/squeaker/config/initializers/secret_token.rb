65908883bd701c0a55e5ae0d9537ff0b2fff4ed8
Yong Bakos <ybakos@humanoriented.com>
2012-08-16 10:42:17 -0600

    Adding rails app for chapter 14.1

---
+++ 14/squeaker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Squeaker::Application.config.secret_token = '367d9adbab7a003dd4b00d8760570c0326e7f67a1863d6120cb5a52d66ec46692f991c6c58af67b5fdb7ed8e46c1ffa628a88e901a9fe705e82b329180a0aac2'
