e671c910d696b6608878886c33bdc0342baf3d6c
Yong Bakos <ybakos@humanoriented.com>
2013-02-04 10:25:42 -0700

    Refactoring step defs to use Aruba methods.

--- 16/calculator/features/step_definitions/calculator_steps.rb
+++ 16/calculator/features/step_definitions/calculator_steps.rb
@@ -1,35 +1,19 @@
 Given /^the input "(.*?)"$/ do |input|
-  steps %{
-    Given a file named "input.txt" with:
-      """
-      #{input}
-      """
-  }
+  write_file "input.txt", input
 end
 
 When /^the calculator is run$/ do
-  steps %{
-    When I run `calculator input.txt`
-  }
+  run "calculator input.txt"
 end
 
 Then /^the output should be "(.*?)"$/ do |output|
-  steps %{
-    Then it should pass with:
-      """
-      #{output}
-      """
-  }
+  assert_passing_with output
 end
 
 When /^the calculator is run with no input$/ do
-  steps %{
-    When I run `calculator` interactively
-  }
+  run_interactive "calculator"
 end
 
 When /^I enter the calculation "([^"]*)"$/ do |calculation|
-  steps %{
-    When I type "#{calculation}"
-  }
+  type calculation
 end
